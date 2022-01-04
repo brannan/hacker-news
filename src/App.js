9c4b9951a181f3b3af20640a8b534653f9d9c622
Patrick Brannan <pbrannan@camptree.org>
2022-01-03 20:17:25 -0600

    SearchForm

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
       const result = await axios.get(url)
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.hits
+        payload: result.data.hits,
       })
     } catch {
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
@@ -79,24 +79,20 @@ const App = () => {
     setSearchTerm(event.target.value)
   }
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`)
+    event.preventDefault()
   }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={handleSearchInput}
-        isFocused
-      >
-        Search:
-      </InputWithLabel>
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
       <hr />
       {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading ? (
@@ -107,6 +103,22 @@ const App = () => {
     </div>
   )
 }
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      onInputChange={onSearchInput}
+      isFocused
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+)
 
 const InputWithLabel = ({
   id,
